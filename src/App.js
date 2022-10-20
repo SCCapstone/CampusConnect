fb9133a3bb51ae1e222631c714badf902cc4d349
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 <erikc@email.sc.edu>
2022-10-20 09:08:18 -0400

    refactor

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { StatusBar } from "react-native";
+import * as React from "react";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { LoginScreen, RegisterScreen, WelcomeScreen } from "./Login.js";
+import { HomeScreen } from "./HomeScreen.js";
+
+import * as Firebase from "./Firebase";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RegisterScreen"
+          component={RegisterScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/Login.js
@@ -0,0 +1,207 @@
+import { Modal, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+} from "react-native";
+
+import * as Firebase from './Firebase'
+import auth from '@react-native-firebase/auth';
+
+import styles from './loginStyles';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+
+export function WelcomeScreen({navigation}) {
+
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  if (user) {
+    navigation.navigate('HomeScreen')
+  }
+  
+ 
+  return (
+    <View style={styles.container}>
+      <Image style={styles.imageLarge} source={require("./assets/gamecock.png")} />
+      <Text style= {styles.title}>Campus Connect</Text>
+
+      <TouchableOpacity onPress={ () => navigation.navigate('RegisterScreen')} style={styles.loginBtn}>
+        <Text style={styles.loginText}>REGISTER</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={ () => navigation.navigate('LoginScreen')} style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.copyWrightText}>Copywright Ⓒ2022 DemBoyz</Text>
+      </View>
+    </View>
+  );
+}
+
+ 
+export function LoginScreen({ navigation}) {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const [isModalVisible, setModalVisible] = React.useState(false);
+  const handleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+ 
+  return (
+    <View style={styles.container}>
+      <BackButton/>
+      <Image style={styles.imageSmall} source={require("./assets/gamecock.png")} />
+      <Text style= {styles.title}>Campus Connect</Text>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="USC Email"
+          textContentType='username'
+          autoCorrect = {false}
+          autoCapitalize = {"none"}
+          autoComplete = {"email"}
+          placeholderTextColor="#000000"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+ 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          textContentType={"password"}
+          autoComplete = {"password"}
+          placeholderTextColor="#000000"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+ 
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={ () => navigation.navigate('RegisterScreen')} style={styles.loginBtn}>
+        <Text style={styles.loginText}>REGISTER</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress= {() => LoginAlert({email})} style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.copyWrightText}>Copywright Ⓒ2022 DemBoyz</Text>
+      </View>
+    </View>
+  );
+}
+
+export function RegisterScreen({ navigation}){
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [password2, setPassword2] = React.useState("");
+  const [modalVisible, setModalVisible] = React.useState(false);
+
+  function register () {
+    Firebase.registerAccount(email,password)
+  }
+
+ 
+  return (
+    <View style={styles.container}>
+      <BackButton/>
+      <Image style={styles.imageSmall} source={require("./assets/gamecock.png")} />
+      <Text style= {styles.title}>Campus Connect</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="USC Email"
+          textContentType='username'
+          autoCorrect = {false}
+          autoCapitalize = {"none"}
+          autoComplete = {"email"}
+          placeholderTextColor="#000000"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+ 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          textContentType={"newPassword"}
+          autoComplete = {"password-new"}
+          placeholderTextColor="#000000"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Confirm Password"
+          textContentType={"newPassword"}
+          autoComplete = {"password-new"}
+          placeholderTextColor="#000000"
+          secureTextEntry={true}
+          onChangeText={(password2) => setPassword2(password)}
+        />
+      </View>
+ 
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={register} style={styles.loginBtn}>
+        <Text style={styles.loginText}>REGISTER</Text>
+      </TouchableOpacity>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.copyWrightText}>Copywright Ⓒ2022 DemBoyz</Text>
+      </View>
+    </View>
+  );
+}
+
+const BackButton = () => {
+  const navigation = useNavigation();
+  return (
+      <TouchableOpacity style={styles.backButtonContainer} onPress={ () => navigation.goBack()}>
+        <ImageBackground style={styles.backButtonImage} source={require("./assets/back_arrow.png")} />
+      </TouchableOpacity>
+  )
+}
+
+const LoginAlert = ({email}) => {
+    Alert.alert('Logged in!', "Successfully logged in " + email, [
+      { text: "OK"}
+    ] );
+
+}
