8c7d3a8ffba4a65aff102e3fea3ec0bc0bf1447c
Neekon Sarmadi <nsarmadi@email.sc.edu>
2022-10-22 19:05:21 -0400

    started events page

---
+++ src/EventScreen.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
---
+++ src/eventStyles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#73000a",
+      alignItems: "center",
+      justifyContent: "center",
+    },
