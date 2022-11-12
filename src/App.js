8794b06aab1e92288e8f990897c6a57cd911cc8f
Timothy Kranz <tkranz@email.sc.edu>
2022-11-12 02:10:19 -0500

    created default screens and edited drawer

--- src/App.js
+++ src/App.js
@@ -20,7 +20,12 @@ import {LoginScreen, RegisterScreen, WelcomeScreen} from './LoginScreens.js'
 import { RegistrationScreen } from './RegistrationScreen.js';
 import { HomeScreen } from './HomeScreen.js';
 import { EventScreen } from './EventScreen.js';
+import { ClassesScreen } from './ClassesScreen.js';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { ProfileScreen } from './ProfileScreen.js';
+import { AddPostScreen } from './AddPostScreen.js';
+
+import { DrawerContent } from './DrawerContent.js';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -28,9 +33,19 @@ const Drawer = createDrawerNavigator();
 
 function Root() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}
+      screenOptions={{
+        headerTintColor: '#ffffff',
+        headerStyle: {backgroundColor: '#73000a'},
+        headerColor: '#73000a',  
+        drawerActiveBackgroundColor: '#73000a',
+        drawerActiveTintColor: 'white'
+      }}>
       <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="View Profile" component={ProfileScreen} />
+      <Drawer.Screen name="Classes" component={ClassesScreen} />
       <Drawer.Screen name="Event" component={EventScreen} />
+      <Drawer.Screen name="Add Post" component={AddPostScreen} />
     </Drawer.Navigator>
   );
 }
@@ -39,19 +54,34 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen
-          name="Root"
-          component={Root}
-          options={{headerShown: false}}
+        <Stack.Screen
+            name="Root"
+            component={Root}
+            options={{headerShown: false}}
         />
-      <Stack.Screen
-          name="WelcomeScreen"
-          component={WelcomeScreen}
-          options={{headerShown: false}}
+        <Stack.Screen
+            name="WelcomeScreen"
+            component={WelcomeScreen}
+            options={{headerShown: false}}
+        />
+        <Stack.Screen
+            name="ClassesScreen"
+            component={ClassesScreen}
+            options={{headerShown: false}}
+        />
+        <Stack.Screen
+            name="AddPostScreen"
+            component={AddPostScreen}
+            options={{headerShown: false}}
         />
         <Stack.Screen
           name="LoginScreen"
           component={LoginScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="ProfileScreen"
+          component={ProfileScreen}
           options={{headerShown: false}}
         />
         <Stack.Screen
--- src/ClassesScreen.js
+++ src/ClassesScreen.js
@@ -4,14 +4,10 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
 
-
-export function HomeScreen({navigation}) {
+export function ClassesScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Event')}
-        title="Go to events"
-      />
-    </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+      </View>
   );
 }
