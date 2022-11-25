029f52783c1d74fd63efc4b90cc1164e57bf16b3
Erik Connerty <erikc@email.sc.edu>
2022-11-25 00:58:44 -0500

    #25 text

--- src/PostsScreen.js
+++ src/PostsScreen.js
@@ -197,7 +197,7 @@ export function PostsScreen({navigation}) {
                         style={styles.postInput}
                         multiline={true}
                         onChangeText={(postText) => setPostText(postText)}
-                        placeholder="Enter ur post"
+                        placeholder="Enter youur post"
                         textAlignVertical='top'
                         placeholderTextColor="black"
                         blurOnSubmit={false}
