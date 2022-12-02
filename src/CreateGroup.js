26e4ac1984bdd56fdb58d4b1f1fa7d27d839a156
Erik Connerty <erikc@email.sc.edu>
2022-12-02 00:19:44 -0500

    actually fix bug create a group overwriting profoile pic

--- src/CreateGroup.js
+++ src/CreateGroup.js
@@ -51,7 +51,7 @@ export function CreateGroup({navigation}) {
   };
 
   const uploadPic = async () => {
-    const reference = storage().ref();
+    const reference = storage().ref(crypto.randomUUID());
     if (image) {
       await reference.putFile(image).catch(error => {
         FirebaseError(error.code);
