92fe6518793e8531b0ce644c3df7fd488e467dc0
Neekon Sarmadi <nsarmadi@email.sc.edu>
2023-03-17 15:54:38 -0400

    adding modal for event description and reformatting UI

--- src/EventsScreen.js
+++ src/EventsScreen.js
@@ -1,118 +1,102 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Alert,
   SafeAreaView,
-  View, 
-  FlatList, 
-  StyleSheet, 
-  Text, 
-  StatusBar, 
-  Image, 
-  Pressable, 
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Image,
+  Pressable,
   TouchableOpacity,
   ActivityIndicator,
- } from 'react-native';
+  Modal,
+} from 'react-native';
 import { ScrapeEventData } from './EventsScraper';
 import iosstyles from './styles/ios/EventsScreenStyles';
 import androidstyles from './styles/android/EventsScreenStyles';
-import {FloatingAction} from 'react-native-floating-action';
+import { FloatingAction } from 'react-native-floating-action';
 
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
-import storage from "@react-native-firebase/storage";
+import storage from '@react-native-firebase/storage';
 
-import ImageView from "react-native-image-viewing";
+import ImageView from 'react-native-image-viewing';
 import FastImage from 'react-native-fast-image';
 import { SearchBar } from '@rneui/themed';
 
 var styles;
- 
+
 if (Platform.OS === 'ios') {
   styles = iosstyles; // do dark mode in here as well
 } else if (Platform.OS === 'android') {
   styles = androidstyles;
 }
 
-
-export function EventsScreen({navigation}) {
+export function EventsScreen({ navigation }) {
   const defaultItemCount = 10;
 
-const [DATA, setDATA] = useState();
+  const [DATA, setDATA] = useState();
 
-useEffect(() => {
-  const fetchData = async () => {
-    const ret = await ScrapeEventData();
-    setDATA(ret);
-    }
-  fetchData();
-}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const ret = await ScrapeEventData();
+      setDATA(ret);
+    };
+    fetchData();
+  }, []);
 
-const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
-    const Event = ({item}) => (
-      
-      <View style={styles.eventContainer}>
-      {/* <View style={styles.upvoteBox}>
-        <TouchableOpacity onPress={() => CreateAlertupVote()}>
-          <Image
-            style={styles.voteButtons}
-            source={
-              item.isUpVoted
-             //   ? require('./assets/upvote_highlighted.png')
-             //   : require('./assets/upvote.png')
-            }></Image>
-        </TouchableOpacity>
-        <Text style={styles.upvote}>{item.upvoteCount}</Text>
-        <TouchableOpacity onPress={() => CreateAlertdownVote()}>
-          <Image
-            style={styles.voteButtons}
-            source={
-              item.isDownVoted
-              //  ? require('./assets/downvote_highlighted.png')
-              //  : require('./assets/downvote.png')
-            }></Image>
-        </TouchableOpacity>
-      </View> */}
-      <TouchableOpacity onPress={() => CreateAlertevent()} style ={styles.event}>
-      <FastImage source={{uri: item.imageUrl}}
-             style={styles.canvas}/>
-            <Text style={styles.body}>{item[0]}</Text>
-            <Text style={styles.date}>{item[2]}</Text>
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  const Event = ({ item }) => (
+    <View style={styles.eventContainer}>
+      <TouchableOpacity onPress={() => { setSelectedEvent(item); setModalVisible(true); }}>
+        <FastImage source={{ uri: item.imageUrl }} style={styles.canvas} />
+        <Text style={styles.body}>{item[0]}</Text>
+        <Text style={styles.date}>{item[2]}</Text>
       </TouchableOpacity>
     </View>
   );
 
+  const renderEvent = ({ item }) => <Event item={item} />;
 
-      const renderEvent = ({ item }) => (
-        <Event item={item} />
-      );
+  if (!DATA) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
 
-    if(!DATA) {
-      return (<View style={{flex:1,justifyContent:'center'}}><ActivityIndicator></ActivityIndicator></View>)
-    }
-
-      return (
-       <SafeAreaView style={styles.container}>
-    <FlatList
-      data={DATA}
-      renderItem={renderEvent}
-      /*keyExtractor={item => item.id}*/ //This is possibly not needed since there is a key already?
-    />
-    {/* <View style={{marginTop: 20}}>
-      <Text>{JSON.stringify(DATA)}</Text>
-    </View> */}
-  </SafeAreaView>
-);
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList data={DATA} renderItem={renderEvent} />
+      <Modal visible={isModalVisible} onRequestClose={() => setModalVisible(false)}>
+        {selectedEvent && (
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>{selectedEvent[0]}</Text>
+            <FastImage source={{ uri: selectedEvent.imageUrl }} style={styles.modalImage} />
+            <Text style={styles.modalDescription}>{selectedEvent[1]}</Text>
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <Text style={styles.modalCloseButton}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Modal>
+    </SafeAreaView>
+  );
 }
 
 const CreateAlertupVote = () => {
   Alert.alert('This will upvote');
-}
+};
 const CreateAlertdownVote = () => {
   Alert.alert('This will downvote');
-}
+};
 const CreateAlertevent = () => {
   Alert.alert('This will hold event info');
-}
-     
-    
+};
