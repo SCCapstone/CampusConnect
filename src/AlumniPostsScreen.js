a9ddd9c28503ddf33f3cd05871039741e68e38ab
Erik Connerty <erikc@email.sc.edu>
2023-04-02 15:57:01 -0400

    alumni posts screen update

--- src/AlumniPostsScreen.js
+++ src/AlumniPostsScreen.js
@@ -8,6 +8,7 @@ import {
   Text,
   TextInput,
   Pressable,
+  FlatList,
   TouchableOpacity,
   ScrollView,
   ActivityIndicator,
@@ -17,7 +18,6 @@ import {
   LayoutAnimation,
   UIManager,
 } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
 import auth, {firebase} from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
@@ -25,6 +25,7 @@ import {FloatingAction} from 'react-native-floating-action';
 import ImageView from 'react-native-image-viewing';
 import moment from 'moment';
 import AppContext from './AppContext';
+import LinearGradient from 'react-native-linear-gradient';
 import {color, sub, ZoomIn} from 'react-native-reanimated';
 import {pure} from 'recompose';
 import FastImage from 'react-native-fast-image';
@@ -32,6 +33,7 @@ import {FlashList} from '@shopify/flash-list';
 import {launchImageLibrary} from 'react-native-image-picker';
 import { useHeaderHeight } from '@react-navigation/elements';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { LinkPreview } from '@flyerhq/react-native-link-preview'
 import {
 Delete,
 useTheme,
@@ -48,7 +50,7 @@ import {v4 as uuidv4} from 'uuid';
 import { AnimatedGalleryImage, LoadingIndicator } from 'stream-chat-react-native';
 
 
-
+import { Freeze } from "react-freeze";
 
 import iosstyles from './styles/ios/PostScreenStyles';
 import iosCommentStyles from './styles/ios/CommentStyles'
@@ -86,6 +88,7 @@ export function AlumniPostsScreen({navigation}) {
   const [modalVisible, setModalVisible] = useState(false);
   const [editModalVisible,setEditModalVisile] = useState(false)
   const [replyModalVisible, setReplyModalVisible] = useState(false);
+  const [freeze, setFreeze] = useState(false);
   const [postText, setPostText] = useState('');
   const [postIsAnonymous,setPostIsAnonymous] = useState(false);
   const [sortMode, setSortMode] = useState('Best')
@@ -131,20 +134,21 @@ export function AlumniPostsScreen({navigation}) {
     }
   };
 
-  const getReplies =  (item) => {
+  const getReplies = async (item) => {
     setRepliesLoading(true)
     const postReplies= [];
     var postsRef = firestore().collection('AlumniPosts').doc(item.key)
     promises = []
     //gets posts asynchronously in the background
-    promise3 = postsRef.get().then( doc => {
+    postsRef.get().then( doc => {
       const replies = doc.get('replies');
       for(firebaseReply of replies){
         var replyRef = firestore().collection('Replies').doc(firebaseReply)
+        promises2 = []
 
         //Create a promise for each request and put it into a promise array. This allows us to load asynchronously
-        promise = replyRef.get().then(reply => {
-          promise2 = firestore().collection('Users').doc(reply.get('user')).get().then( data => {
+        promise = replyRef.get().then(async reply => {
+          return firestore().collection('Users').doc(reply.get('user')).get().then( data => {
             user = ({...data.data()})
             const tempReply = {
               ...reply.data(),
@@ -163,68 +167,72 @@ export function AlumniPostsScreen({navigation}) {
             tempReply.isDownVoted = tempReply.downvoters[auth().currentUser.uid];
             tempReply.postIsYours = tempReply.user === auth().currentUser.uid
             postReplies.push(tempReply);
-        })
 
-          promises.push(promise2)
+          })
+
         });
         promises.push(promise)
 
       }
-
+      Promise.all(promises).then(() => {
+        setPostReplies(postReplies.sort(function(a,b) {return a.date - b.date;}))
+        setRepliesLoading(false)
+        setRefreshList(!refresh)
+      })
 
 
     })
-    promises.push(promise3)
+
     //Wait for all the replies to load
-    Promise.all(promises).then(() => {
+    /*Promise.all(promises).then(() => {
       //The first one will sort by upvote count,then date.
       //setPostReplies(postReplies.sort(function(a,b) {return b.upvoteCount - a.upvoteCount || a.date - b.date;}))
       setPostReplies(postReplies.sort(function(a,b) {return a.date - b.date;}))
       setRepliesLoading(false)
       setRefreshList(!refresh)
-    })
+    })*/
 
 
   
   }
 
   const getPosts = () => {
-    setRefresh(true)
+    setRefresh(true);
     var postsRef = firestore().collection('AlumniPosts')
     var query; 
     if(search) {
       query = postsRef
       .where('searchAuthor', '>=', search.toUpperCase())
       .where('searchAuthor', '<=', search.toUpperCase()+ '\uf8ff')
-      .limit(15)
+      //.limit(15)
     }
     else if(sortMode === 'Best') {
       query = postsRef
       .orderBy('upvoteCount', 'desc')
       .orderBy('date', 'desc')
-      .limit(5)
+      //.limit(5)
     } else if (sortMode === 'Worst') {
       query = postsRef
       .orderBy('upvoteCount', 'asc')
       .orderBy('date', 'desc')
-      .limit(5) 
+      //.limit(5) 
 
     } else if (sortMode === 'New') {
       query = postsRef
       .orderBy('date', 'desc')
-      .limit(5) 
+     // .limit(5) 
 
     } else if (sortMode === 'Anonymous') {
       query = postsRef
       .where('author','==', 'USC Student')
       .orderBy('upvoteCount', 'desc')
       .orderBy('date', 'desc')
-      .limit(5) 
+     // .limit(5) 
     }
     else if (sortMode === 'Most Commented') {
       query = postsRef
       .orderBy('replyCount', 'desc')
-      .limit(5) 
+   //   .limit(5) 
     }
     query.get().then(snapShot => {
       if(!snapShot.metadata.hasPendingWrites) {
@@ -254,7 +262,6 @@ export function AlumniPostsScreen({navigation}) {
                 post.author = 'USC Student'
                 post.pfp = ''
               }
-
               posts.push(post);
               if (post.extraData) {
                 images.push({
@@ -269,6 +276,7 @@ export function AlumniPostsScreen({navigation}) {
           promises.push(promise)
         });
         Promise.all(promises).then(() => {
+          //setRefreshList(!refresh) 
           setPosts(posts);
           setImages(images);
           setLoading(false);
@@ -419,34 +427,34 @@ export function AlumniPostsScreen({navigation}) {
       query = postsRef
       .where('searchAuthor', '>=', search.toUpperCase())
       .where('searchAuthor', '<=', search.toUpperCase()+ '\uf8ff')
-      .limit(15)
+      //.limit(15)
     }
     else if(sortMode === 'Best') {
       query = postsRef
       .orderBy('upvoteCount', 'desc')
       .orderBy('date', 'desc')
-      .limit(postCount)
+     // .limit(postCount)
     } else if (sortMode === 'Worst') {
       query = postsRef
       .orderBy('upvoteCount', 'asc')
       .orderBy('date', 'desc')
-      .limit(postCount) 
+    //  .limit(postCount) 
 
     } else if (sortMode === 'New') {
       query = postsRef
       .orderBy('date', 'desc')
-      .limit(postCount) 
+     // .limit(postCount) 
 
     } else if (sortMode === 'Anonymous') {
       query = postsRef
       .where('author','==', 'USC Student')
       .orderBy('upvoteCount', 'desc')
       .orderBy('date', 'desc')
-      .limit(postCount) 
+   //   .limit(postCount) 
     } else if (sortMode === 'Most Commented') {
       query = postsRef
       .orderBy('replyCount', 'desc')
-      .limit(postCount) 
+ //     .limit(postCount) 
     }
     //gets posts asynchronously in the background
     const subscriber = query.onSnapshot(querySnapshot => {
@@ -500,7 +508,9 @@ export function AlumniPostsScreen({navigation}) {
             Promise.all(promises).then(() => {
               setPosts(posts);
               setImages(images);
+              //setRefreshList(!refresh)
               setLoading(false);
+
             })
 
           }
@@ -693,7 +703,7 @@ export function AlumniPostsScreen({navigation}) {
   }
 
   const uploadPic = async () => {
-    const reference = storage().ref('/Posts/' +uuidv4());
+    const reference = storage().ref('/AlumniPosts/' +uuidv4());
     if (image) {
       await reference.putFile(image).catch(error => {
         FirebaseError(error.code);
@@ -704,7 +714,7 @@ export function AlumniPostsScreen({navigation}) {
 
 
 
-  const Post = ({item, index}) => {
+  const Post = React.memo(({item, index}) => {
     return (
       <Swipeable
       overshootLeft={true}
@@ -713,6 +723,7 @@ export function AlumniPostsScreen({navigation}) {
       }}
       onSwipeableOpen={(direction) => {
         if(direction ==='right'){
+          setFreeze(true)
           setReplyItem(item)
           setReplyModalVisible(true)
           getReplies(item);
@@ -756,6 +767,7 @@ export function AlumniPostsScreen({navigation}) {
     >
         <View style={styles.postContainer}>
           <Pressable delayLongPress={150} onLongPress={() => {
+            setFreeze(true)
             setReplyItem(item)
             setReplyModalVisible(true)
             setPostReplies([])
@@ -769,11 +781,13 @@ export function AlumniPostsScreen({navigation}) {
                   buttonTextAfterSelection={() => {return '• • •'}}
                   onSelect={(option) => {
                     if(option === 'Reply') {
+                      setFreeze(true)
                       setReplyItem(item)
                       setReplyModalVisible(true)
                       getReplies(item);
                     }
                     else if(option === 'Edit') {
+                      setFreeze(true)
                       setPost(item.key);
                       setPostText(item.body)
                       //this.floatingAction.animateButton()
@@ -825,11 +839,13 @@ export function AlumniPostsScreen({navigation}) {
                   buttonTextAfterSelection={() => {return '• • •'}}
                   onSelect={(option) => {
                     if(option === 'Reply') {
+                      setFreeze(true)
                       setReplyItem(item)
                       setReplyModalVisible(true)
                       getReplies(item);
                     }
                     else if(option === 'Edit') {
+                      setFreeze(true)
                       setPost(item.key);
                       setPostText(item.body)
                       //this.floatingAction.animateButton()
@@ -847,8 +863,9 @@ export function AlumniPostsScreen({navigation}) {
                 />:null}
             </View>
             <View style={styles.postImageView}>
-              <Text style={styles.body}>{item.body}</Text>
-              {item.extraData ? (
+              <Text style={styles.body}>{item.body.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '')}</Text>
+              {item.body.search(/(?:https?|ftp):\/\/[\n\S]+/g, '') > 0 ? <LinkPreview containerStyle={{marginTop:20,backgroundColor:'#E7E2E1',borderRadius:20}} renderDescription={((string) => {return (<Text style={{color:'black',fontSize:10}}>{string}</Text>)})} renderTitle={((string) => {return (<Text style={{color:'black',fontWeight:'bold'}}>{string}</Text>)})} renderText={(() => {return ''})} header='' text={item.body} />: null}
+              {(item.extraData && item.body.search(/(?:https?|ftp):\/\/[\n\S]+/g, '') < 1) ? (
                 <TouchableOpacity onPress={() => {OpenImage({index})}}>
                   <FastImage
                     source={{uri: item.extraData}}
@@ -873,7 +890,7 @@ export function AlumniPostsScreen({navigation}) {
         </View>
       </Swipeable>
     );
-  };
+  });
 
 
   const closeModal = () => {
@@ -886,6 +903,7 @@ export function AlumniPostsScreen({navigation}) {
     setPostIsAnonymous(false)
     setModalVisible(false)
     setPostText('');
+    setFreeze(false)
   };
 
   const renderPost = ({item, index}) => <Post item={item} index={index} />;
@@ -934,6 +952,7 @@ export function AlumniPostsScreen({navigation}) {
                 userData.setProfileView(item.user.replace('/Users/',''))
                 navigation.navigate('ProfileView')
                 setReplyModalVisible(false)
+                setFreeze(false)
                 setPostReplies([])
               }
               else if (item.author === 'USC Student') {
@@ -956,11 +975,11 @@ export function AlumniPostsScreen({navigation}) {
       </Swipeable>
 
     )
-  })
+  });
 
   if (loading) {
     return (
-      <View style={styles.activityIndicator}>
+      <View style={{flex:1,justifyContent:'center'}}>
         <ActivityIndicator size="large" />
       </View>
     );
@@ -1073,7 +1092,7 @@ export function AlumniPostsScreen({navigation}) {
                 buttonStyle={{backgroundColor:'white',alignSelf:'flex-start',marginBottom:20,marginLeft:10}}
                 size='lg'
                 onPress={() =>{setReplyModalVisible(false);setPostReplies([])
-                setReplyItem(null)}}
+                setReplyItem(null);setFreeze(false);}}
                 titleStyle={{fontSize:15,fontWeight:'bold',color:'black'}}
                 title={'Close'}
               />
@@ -1086,6 +1105,7 @@ export function AlumniPostsScreen({navigation}) {
                 isChecked={postIsAnonymous}
                 unfillColor="#FFFFFF"
                 text="Anonymous?"
+      
                 iconStyle={{ borderColor: "#73000a"}}
                 innerIconStyle={{ borderWidth: 2 }}
                 textStyle={{ color:'black' }}
@@ -1126,27 +1146,30 @@ export function AlumniPostsScreen({navigation}) {
 
       </Modal>
 
-
-      <FlashList
-       onRefresh={() => {getPosts}}
-        onEndReached={() => {
-          setPostCount(postCount+6)
-        }}
-        onEndReachedThreshold={.77}
-        data={posts}
-        ref={list}
-        key={refresh}
-        renderItem={renderPost}
-        keyExtractor={item => item.key}
-        refreshing={refreshing}
-        estimatedItemSize={100}
-      />
+      <Freeze freeze={freeze}>
+        <FlatList
+        onRefresh={() => {getPosts}}
+          //onEndReached={() => {
+        //   setPostCount(postCount+6)
+        // }}
+        //  onEndReachedThreshold={.9}
+          data={posts}
+          ref={list}
+          key={refresh}
+          renderItem={renderPost}
+          keyExtractor={item => item.key}
+          refreshing={refreshing}
+          initialNumToRender={100}
+          
+        />
+      </Freeze>
       <FloatingAction
         color="#73000a"
         ref={ref => {
           this.floatingAction = ref;
         }}
         onPressMain={() => {
+          setFreeze(true)
           setModalVisible(true);
         }}
 
