d449297412a1a55d6686aee1b48a5929d4fd7588
Erik Connerty <erikc@email.sc.edu>
2023-04-23 13:53:41 -0400

    concert

--- src/EventsScraper.js
+++ src/EventsScraper.js
@@ -71,6 +71,15 @@ const LoadEvents = async () => {
   });
 
   await Promise.all(promises);
+  events.unshift({
+    title: 'U of SC Campus Recreation Presents: The Kid Laroi',
+    date: 'Friday, Aug 8, 7:00PM',
+    location: 'Colonial Life Arena',
+    price: 'USC Alumni Association',
+    imageUrl: 'https://www.bleed4you.com/images/tour_square-rev.jpg',
+    description: 'Come join us at Colonial Life Arena for a free student concert with The Kid Laroi!',
+  }
+  );
   return events.filter(Boolean);
 
 };
