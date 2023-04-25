38178312771c34c5accb7950cdab353f6b510806
Neekon Sarmadi <nsarmadi@email.sc.edu>
2023-04-24 20:24:12 -0400

    added comments

--- src/EventsScraper.js
+++ src/EventsScraper.js
@@ -1,36 +1,47 @@
+//Import required packages. 
 import React from 'react';
 import {useState} from 'react';
 import uuid from 'react-native-uuid';
 import moment, { invalid } from 'moment';
 
+//Import axios, allows for HTTP requests.
 import axios from 'axios';
+//Import axios, allows for web scraping
 const cheerio = require('react-native-cheerio');
+
+//Define URL to be scraped and create a default logo for events.
 const url = 'https://www.eventbrite.com/o/university-of-south-carolina-alumni-association-18391111883';
 const DEFAULTEVENTLOGO =
   'https://abcnews4.com/resources/media/f4369747-6ca4-496c-9243-5e9e9a0f3089-large16x9_UniversityofSouthCarolinaFormalLogo16x9.jpg?1599583281911';
+
+  //Define fucntion being exported
 export async function ScrapeEventData() {
   try {
-    return await LoadEvents();
+    return await LoadEvents(); //Call LoadEvents funtion and return the result
   } catch (error) {
-    console.log('error');
+    console.log('error'); //Log any errors 
   }
 }
-
+//Define the format of the date string to be used in scraping
 const formatString = 'ddd, MMM D, h:mm A';
+
+//Define the load events function. 
 const LoadEvents = async () => {
   const currentDate = new Date();
 
+  //Fetch the HTML content of the url
   const response = await fetch(url);
   const htmlString = await response.text();
   const $ = cheerio.load(htmlString);
 
-  const promises = [];
-  const uniqueEventIdentifiers = new Set();
+  const promises = []; //Initialize array to store promises
+  const uniqueEventIdentifiers = new Set(); // Initialize a set to store unique event indentifiers
 
-  titleList = $(".article.eds-event-card-content");
+  titleList = $(".article.eds-event-card-content"); //Get list of all event titles
 
-  events = new Array(titleList.length);
+  events = new Array(titleList.length); //Intialize an array to store event objects based on the number of titles
 
+  //Loop through each event article. 
   $('article.eds-event-card-content').each((index, element) => {
     const title = $(element).find('.eds-event-card-content__title').text().trim();
     const halfway = Math.floor(title.length / 2);
@@ -43,11 +54,12 @@ const LoadEvents = async () => {
     const imageUrl = $(element).find('.eds-event-card-content__image').attr('src');
     const eventDate = moment(date, 'ddd, MMM D, h:mm A').toDate();
 
-    const eventIdentifier = firstHalf + '|' + date;
+    const eventIdentifier = firstHalf + '|' + date; //Create a unique event identifier
 
+    //Checks that the event has a title, occurs in the future, and has not already been added
     if (title && (currentDate < eventDate || eventDate.toUTCString() === 'Invalid Date') && !uniqueEventIdentifiers.has(eventIdentifier)) {
       uniqueEventIdentifiers.add(eventIdentifier);
-
+      //Return filtered events array
       const promise = fetch(link)
         .then(response => response.text())
         .then(eventHtml => {
